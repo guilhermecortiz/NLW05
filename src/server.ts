be31e0b683cd890c5ad0e4df70b5aa0612dcabd1
guilhermecortiz <guilhermecortiz@gmail.com>
2021-04-26 05:15:13 -0300

    Class 03

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+import path from 'path';
+
 import './database';
 import { routes } from './routes';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'public'));
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'html');
+
+app.get('/', (request, response) => response.render('html/client.html'));
+
+const http = createServer(app);
+const oi = new Server(http);
+
+oi.on('connection', (socket: Socket) => console.log('Se conectou', socket.id));
+
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => console.log('Server is runnig on port 3333'));
+http.listen(3333, () => console.log('Server is runnig on port 3333'));
